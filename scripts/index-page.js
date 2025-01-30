748cf039c36b21051ec3e15ba2b5da65dfa5656a
Yawen Diao <yawendiao@gmail.com>
2025-01-30 01:07:26 -0500

    submit form

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -7,9 +7,9 @@ console.log(image);
 
 // create an array to contain existing comments.
 const comments = [
-{name: "Victor pinto", date: "11/02/2023", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
-{name: "Christina Cabrera", date: "10/28/2023", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day. "},
 {name: "Isaac Tadesse", date: "10/20/2023", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."},
+{name: "Christina Cabrera", date: "10/28/2023", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day. "},
+{name: "Victor pinto", date: "11/02/2023", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
 ]
 
 function displayComments(comment){
@@ -32,9 +32,27 @@ function renderComments(){
   const myComment = document.querySelector("comments-list");
   //clear the comments
   myComment.innerHTML="";
-}
+  for (let i =2; i >= 0; i--){
+    const card =displayComments(comments[i]);
+    myComment.appendChild(card);
+  }
+};
+
+function handleFormSubmit(e){
+  e.preventDedault();
+
+};
 
 
+const commentData= {
+  name:e.target.name.value,
+  comment:e.target.comment.value,
+};
 
+comments.push(commentData);
+comments.pop();
+renderComments();
 
-
+const formEl = document.querySelector(".main__form");
+formEl.addEventListener("submit",handleFormSubmit);
+renderComments();
