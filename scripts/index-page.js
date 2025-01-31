9cf63bf4f4a157e6167d57c679027e21cb840a8c
Yawen Diao <yawendiao@gmail.com>
2025-01-30 20:27:01 -0500

    comment udpted

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -7,16 +7,28 @@ console.log(image);
 
 // create an array to contain existing comments.
 const comments = [
-  {name: "Victor pinto", date: "11/02/2023", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
-  {name: "Christina Cabrera", date: "10/28/2023", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day. "},
-  {name: "Isaac Tadesse", date: "10/20/2023", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."},
-]
+  { 
+    name: "Victor pinto", 
+    date: "11/02/2023", 
+    comment: "This is art. Expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+  },
+  { 
+    name: "Christina Cabrera", 
+    date: "10/28/2023", 
+    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+  },
+  { 
+    name: "Isaac Tadesse", 
+    date: "10/20/2023", 
+    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+  }
+];
 
+// diplay function
+function displayComments(comments){
+  const articles = [];
 
-
-function displayComments(comment){
-  for(let i=0; i<=2;i ++){
-
+  for(let i =0; i <comments.length; i++){
     const cardEl = document.createElement("article");
     cardEl.classList.add("main__card")
     
@@ -26,7 +38,7 @@ function displayComments(comment){
     const cardContainer = document.createElement("div");
     
     const heading = document.createElement("h2");
-    heading.textContent = comments[i].name;
+    heading.textContent = comments[i].name
     
     const text = document.createElement('p');
     text.textContent = comments[i].comment;
@@ -38,42 +50,57 @@ function displayComments(comment){
     cardEl.appendChild(date);//date
     cardContainer.appendChild(heading);//name
     cardContainer.appendChild(text);//comment
-    return cardEl
-  } //return the whoe element(variable already)
-}
+    
+    articles.push(cardEl);
+  }
+  return articles;
+  }
 
+// submit form
+const formEl = document.querySelector(".main__form");
+const myComment = document.querySelector("#comments-list");
 
+formEl.addEventListener("submit", function(event){
+  event.preventDefault();
 
+//toch the form values
+  const newComment = event.target.comment.value;
+  const newName = event.target.name.value; 
 
-// function renderComments(){
-  // const myComment = document.querySelector("#comments-list");
-  // clear the comments
-  // myComment.innerHTML="";
-  // for (let i =2; i >= 0; i--){
-    // const card =displayComments(comments[i]);
-    // myComment.appendChild(card);
-  // }
-// };
+//check if the form is filled out
+   if(!newComment.trim() || !newName.trim()){
+    alert("Please fill out the form");
+    return;
+   }
 
-// everytime the user submits the form, the funcation is called and data is sent to the server.
-function handleFormSubmit(e){
-  e.preventDefault();
-  const newComment = e.target.comment.value;
-  const newName = e.target.name.value;
-  return {name:newName, comment:newComment}
-  const commentData ={name:newName, comment:newComment, date: new Date().toLocaleDateString()};
-};
+   console.log("newName", newName);
+   console.log("newComment", newComment);
+// create a new comment object
+  const commentData = {
+    name:newName, 
+    date: new Date().toLocaleDateString(),
+    comment:newComment
+  };
 
-comments.unshift(commentData);
-comments.pop();
-console.log(displayComments(commentData));
+  console.log("commentData", commentData);
 
-//reset input fields how to make sure that it resets after the submit
-// e.target.name.value ="";
-// e.target.comment.value = "";
-// 
-// const formEl = document.querySelector(".main__form");
-// 
-// formEl.addEventListener("submit",handleFormSubmit);
-// renderComments();
-// 
+  comments.unshift(commentData);
+  console.log("comments", comments);
+
+  myComment.innerHTML = "";
+
+  const updatedComments = displayComments(comments);
+  updatedComments.forEach(comment => {
+    myComment.appendChild(comment);
+  })
+
+//clear the form
+  formEl.reset();
+
+});
+
+const commentSection = displayComments(comments);
+commentSection.forEach(comment =>{
+  myComment.appendChild(comment);
+})
+
