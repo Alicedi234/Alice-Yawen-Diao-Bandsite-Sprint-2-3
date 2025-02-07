dbe79c91a6d900298d9d7e1da558a8115d4db869
Yawen Diao <yawendiao@gmail.com>
2025-02-07 14:51:06 -0500

    bio completed

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,36 +1,3 @@
-// const image =document.createElement("img");
-// image.src= "./assets/Images/Mohan-muruge.jpg";
-// image.classList = "main__image--item";
-// const container = document.querySelector(".main__image");
-// container.appendChild(image);
-// console.log(image);
-
-
-
-
-
-// async function postComment(commentData){
-  // const url =`${this.baseUrl}${path}?api_key=${this.apiKey}`;
-  // try{
-    // // const reponsePost = await axios.post("https://unit-2-project-api-25c1595833b2.herokuapp.com/comments?api_key=64eebdbe-732a-4d0c-80e7-c53f51613e18",
-    // commentData, {Headers: {"content-Type":"application/json"}});
-    // console.log(reponsePost.data);
-    // return reponsePost.data;
-      // 
-  // }catch(error){
-    // console.log(error);
-  // 
-// }
-// }
-
-
-
-//reset data
-
-
-
-
-
 class BandSiteApi{
   constructor(apiKey){
     this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
@@ -49,137 +16,30 @@ class BandSiteApi{
     }
   }
 // get comments
-
-async getData(path){
+async getComments(path){
   const url =`${this.baseUrl}${path}?api_key=${this.apiKey}`;
   try{
     const response = await axios.get(url);
     const nameSingle = response.data.map(item => item.name);
     const commentSingle = response.data.map(item => item.comment);
     const dateSingle = response.data.map(item => item.timestamp);
-    
+    console.log(response.data);
+    console.log(nameSingle, commentSingle, dateSingle);
     return {nameSingle, commentSingle, dateSingle}
   }catch (error){
 console.log(error);
   }
   }
+
+// get shows
+async getShows(){
+  const url = `${this.baseUrl}showdates?api_key=${this.apiKey}`;
 }
 
-const apiKey = "64eebdbe-732a-4d0c-80e7-c53f51613e18";
-const api = new BandSiteApi(apiKey);
-
-// form submit
-const container = document.querySelector("#comments-list");
-const formEl = document.querySelector(".main__form");
-
-formEl.addEventListener("submit", async function(event){
-  event.preventDefault();
-  const  NewName = event.target.name.value;
-  const NewComment = event.target.comment.value;
-
-//check if the input is valid
-  if(!NewComment.trim() || !NewName.trim()){
-    const inputEl = document.querySelector("#name");
-    inputEl.style.border = "2px solid #D22D2D";
-    
-    const inputEl2 = document.querySelector("#comment");
-    inputEl2.style.border = "2px solid #D22D2D";
-    alert("Please fill out the form");
-   return;
-  }
-  console.log("NewName", NewName);
-  console.log("NewComment", NewComment);
-
-  const commentData = {
-    name: NewName,
-    comment: NewComment,
-  }
-  const responseData = await api.postComment(commentData);
-  console.log(responseData);
   
-  return await DisplayComments();
-})
-
-
-
-
-
-
-async function DisplayComments(){
-  const dataGot = await api.getData("comments");
-  container.innerHTML = "";
-  console.log(dataGot);
-  // const articles = [];
-  for (let i = 0; i < dataGot.nameSingle.length; i++){
-    const cardEl = document.createElement("article");
-    cardEl.classList.add("main__card");
-    
-    const pic = document.createElement('div');
-    pic.classList.add("main__avatar");
-    
-    const cardContainer2 = document.createElement("div")
-    cardContainer2.classList.add("main__container2");
-    
-    const cardContainer3 = document.createElement("div")
-    cardContainer3.classList.add("main__container3");
-    
-    const headingName = document.createElement("h2");
-    headingName.classList.add("main__container2--heading")
-    headingName.textContent = dataGot.nameSingle[i];
-    
-    const textComment = document.createElement('p');
-    textComment.textContent = dataGot.commentSingle[i];
-    textComment.classList.add("main__container2--text");
-    
-    const date = document.createElement('p');
-    date.classList.add("main__date");
-    date.textContent = dataGot.dateSingle[i];
-    
-    const dividerBottom = document.createElement('hr');
-    dividerBottom.classList.add("main__divider");
-    
-    cardContainer2.appendChild(headingName);//name
-    cardContainer2.appendChild(textComment);//comment
-    cardContainer3.appendChild(pic);
-    cardContainer3.appendChild(cardContainer2);
-    cardEl.appendChild(cardContainer3);
-    cardEl.appendChild(date);//date
-    cardEl.appendChild(dividerBottom);
-    
-    // articles.push(cardEl);
-    container.appendChild(cardEl);
-  }
-  console.log(container);
-  return container;
 }
 
-DisplayComments();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
+export default BandSiteApi;
 
 
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,173 +1,94 @@
 const image =document.createElement("img");
 image.src= "./assets/Images/Mohan-muruge.jpg";
 image.classList = "main__image--item";
-const container = document.querySelector(".main__image");
-container.appendChild(image);
-console.log(image);
-
-// create a new variable to store the constant APikey
-const API_KEY = "64eebdbe-732a-4d0c-80e7-c53f51613e18";
-
-// create an instance of BandSiteApi
-const api = new BandSiteApi(API_KEY);
-console.log("new instance", api);
-
-console.log(await api.getData("comments"));
+const containerImage = document.querySelector(".main__image");
+containerImage.appendChild(image);
+console.log(containerImage);
 
 
+import BandSiteApi from "./band-site-api.js"
 
+const apiKey = "64eebdbe-732a-4d0c-80e7-c53f51613e18";
+const api = new BandSiteApi(apiKey);
+// form submit
+const container = document.querySelector("#comments-list");
+const formEl = document.querySelector(".main__form");
 
+formEl.addEventListener("submit", async function(event){
+  event.preventDefault();
+  const  NewName = event.target.name.value;
+  const NewComment = event.target.comment.value;
 
-
-
-
-
-
-
-
-
-
-
-// create an array to contain existing comments.
-
-
-const comments = [
-  { 
-    name: "Victor Pinto", 
-    date: "11/02/2023", 
-    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-  },
-  { 
-    name: "Christina Cabrera", 
-    date: "10/28/2023", 
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-  },
-  { 
-    name: "Isaac Tadesse", 
-    date: "10/20/2023", 
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+//check if the input is valid
+  if(!NewComment.trim() || !NewName.trim()){
+    const inputEl = document.querySelector("#name");
+    inputEl.style.border = "2px solid #D22D2D";
+    
+    const inputEl2 = document.querySelector("#comment");
+    inputEl2.style.border = "2px solid #D22D2D";
+    alert("Please fill out the form");
+   return;
   }
-];
+  console.log("NewName", NewName);
+  console.log("NewComment", NewComment);
 
-// I tried it ... didn't work :(
+  const commentData = {
+    name: NewName,
+    comment: NewComment,
+  }
+  const responseData = await api.postComment(commentData);
+  console.log(responseData);
+  
+  formEl.reset();
+  return await DisplayComments();
+})
 
-// function timeAgo(dateString) {
-//   const now = new Date().toISOString();
-//   const past = new Date(dateString);
-//   const diffInSeconds = Math.floor((now - past) / 1000);
-
-//   if (diffInSeconds < 60) {
-//       return `${diffInSeconds} seconds ago`;
-//   } else if (diffInSeconds < 3600) {
-//       return `${Math.floor(diffInSeconds / 60)} minutes ago`;
-//   } else if (diffInSeconds < 86400) {
-//       return `${Math.floor(diffInSeconds / 3600)} hours ago`;
-//   } else if (diffInSeconds < 604800) {
-//       return `${Math.floor(diffInSeconds / 86400)} days ago`;
-//   } else{
-//     return past.toLocaleDateString();
-//   }
-// }
-
-
-// diplay function
-function displayComments(comments){
-  const articles = [];
-
-  for(let i =0; i <comments.length; i++){
+async function DisplayComments(){
+  const dataGot = await api.getComments("comments");
+  container.innerHTML = "";
+  console.log(dataGot);
+  // const articles = [];
+  for (let i = ((dataGot.nameSingle.length) - 1); i >= 0; i--){
     const cardEl = document.createElement("article");
     cardEl.classList.add("main__card");
     
     const pic = document.createElement('div');
     pic.classList.add("main__avatar");
     
-    const cardContainer2 = document.createElement("div");
+    const cardContainer2 = document.createElement("div")
     cardContainer2.classList.add("main__container2");
-
-    const cardContainer3 = document.createElement("div");
+    
+    const cardContainer3 = document.createElement("div")
     cardContainer3.classList.add("main__container3");
     
-    const heading = document.createElement("h2");
-    heading.classList.add("main__container2--heading")
-    heading.textContent = comments[i].name;
+    const headingName = document.createElement("h2");
+    headingName.classList.add("main__container2--heading")
+    headingName.textContent = dataGot.nameSingle[i];
     
-    const text = document.createElement('p');
-    text.textContent = comments[i].comment;
-    text.classList.add("main__container2--text");
-
+    const textComment = document.createElement('p');
+    textComment.textContent = dataGot.commentSingle[i];
+    textComment.classList.add("main__container2--text");
+    
     const date = document.createElement('p');
     date.classList.add("main__date");
-    date.textContent =comments[i].date;
+    date.textContent = new Date(dataGot.dateSingle[i]).toLocaleDateString();
     
-    // date.textContent =timeAgo(comments[i].date);
-    
-
     const dividerBottom = document.createElement('hr');
     dividerBottom.classList.add("main__divider");
-
-    cardContainer2.appendChild(heading);//name
-    cardContainer2.appendChild(text);//comment
+    
+    cardContainer2.appendChild(headingName);//name
+    cardContainer2.appendChild(textComment);//comment
     cardContainer3.appendChild(pic);
     cardContainer3.appendChild(cardContainer2);
     cardEl.appendChild(cardContainer3);
     cardEl.appendChild(date);//date
     cardEl.appendChild(dividerBottom);
-  
-    articles.push(cardEl);
+    
+    // articles.push(cardEl);
+    container.appendChild(cardEl);
   }
-  return articles;
-  }
+  console.log(container);
+  return container;
+}
 
-// submit form
-const formEl = document.querySelector(".main__form");
-const myComment = document.querySelector("#comments-list");
-
-formEl.addEventListener("submit", function(event){
-  event.preventDefault();
-
-//toch the form values
-  const newComment = event.target.comment.value;
-  const newName = event.target.name.value; 
-
-//check if the form is filled out
-   if(!newComment.trim() || !newName.trim()){
-     const inputEl = document.querySelector("#name");
-     inputEl.style.border = "2px solid #D22D2D";
-
-     const inputEl2 = document.querySelector("#comment");
-     inputEl2.style.border = "2px solid #D22D2D";
-     alert("Please fill out the form");
-    return;
-   }
-
-   console.log("newName", newName);
-   console.log("newComment", newComment);
-// create a new comment object
-  const commentData = {
-    name:newName, 
-    date: new Date().toLocaleDateString(),
-    comment:newComment,
-  };
-
-  console.log("commentData", commentData);
-
-  comments.unshift(commentData);
-  console.log("comments", comments);
-
-  myComment.innerHTML = "";
-
-  const updatedComments = displayComments(comments);
-  updatedComments.forEach(comment => {
-    myComment.appendChild(comment);
-  })
-
-//clear the form
-  formEl.reset();
-
-});
-
-const commentSection = displayComments(comments);
-commentSection.forEach(comment =>{
-  myComment.appendChild(comment);
-})
-
+DisplayComments();
